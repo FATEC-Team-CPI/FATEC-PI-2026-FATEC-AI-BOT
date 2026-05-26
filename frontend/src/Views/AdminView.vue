9b4ef866e610416540bd022f55b32af32e28fff9
LuisTakeo <LuisTakeo@users.noreply.github.com>
2026-05-26 19:23:20 +0000

    chore(sync): frontend from FATEC-Team-CPI/FATEC-AI-BOT-FrontEnd-Vue@630bd5beb47e6b6e580839ae1c6064e719851de7 source_ref: develop triggered_by: LuisTakeo workflow_run: https://github.com/FATEC-Team-CPI/FATEC-AI-BOT-FrontEnd-Vue/actions/runs/26470037836

---
+++ frontend/src/Views/AdminView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-container app-container-screen">
+    <h1 class="app-title adm">MENU ADMINISTRATIVO</h1>
+
+    <div class="app-form">
+      <router-link to="docs" class="adm-btn" tag="button">
+        <section class="adm-content">
+        <img class="icon-cog" src="../assets/Vector.png" alt="">
+        <p>Adicionar Documentos</p>
+      </section>
+      </router-link>
+
+      <router-link to="register" class="adm-btn" tag="button">
+        <section class="adm-content">
+          <img class="icon-cog" src="../assets/Vector.png" alt="">
+          <p>Cadastrar um Novo Usuário</p>
+        </section>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped src="../assets/admin.css"></style>
---
+++ frontend/src/Views/RegisterView.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-link to="/menu" tag="button" class="seta"><img src="../assets/seta.png" alt=""></router-link>
+  <div class="app-container app-container-bar">
+    <section>
+      <h1 class="app-title">CADASTRO</h1>
+      <h2 class="app-subtitle sub">Insira as informações para criar uma nova conta</h2>
+    </section>
+
+    <div class="app-form">
+      <div class="form-group">
+        <label>Campo 1: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 2: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 3: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 4: *</label>
+        <input  class="input"type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 5: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 6: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 7: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+
+      <div class="form-group">
+        <label>Campo 8: *</label>
+        <input class="input" type="text" placeholder="Digite..." />
+      </div>
+    </div>
+
+    <button class="form-btn">Registrar</button>
+  </div>
+</template>
+
+<style scoped src="../assets/register.css"></style>
